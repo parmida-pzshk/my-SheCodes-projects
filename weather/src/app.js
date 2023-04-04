ab325dbb1a01cd9afe4e86dad869f6349c1dc407
parmida-pzshk <pezeshkiparmida@gmail.com>
2023-04-04 12:51:17 +0330

    completed

--- weather/src/app.js
+++ weather/src/app.js
@@ -26,8 +26,10 @@ let name = document.querySelector(".name");
 let tempe = document.querySelector(".number");
 let wind = document.querySelector(".wind");
 let description = document.querySelector(".description");
+let humidity = document.querySelector(".humidity");
 let icon = document.querySelector(".icon");
 let imgDiv = document.getElementById("img"); // access the image using id
+let celsiusDeg = null;
 
 function searchCity(event) {
   event.preventDefault();
@@ -40,17 +42,22 @@ function searchCity(event) {
   function temp(response) {
     let currTemp = Math.round(response.data.main.temp);
     console.log(response.data);
+    celsiusDeg = response.data.main.temp;
     let tempe = document.querySelector(".number");
     let wind = document.querySelector(".wind");
     tempe.innerHTML = currTemp;
     let currWind = Math.round(response.data.wind.speed);
     wind.innerHTML = currWind;
-    description.innerHTML = response.data.weather[0].description;
+    let editedDescription =
+      response.data.weather[0].description.charAt(0).toUpperCase() +
+      response.data.weather[0].description.slice(1);
+    description.innerHTML = editedDescription;
     let iconNew = response.data.weather[0].icon;
-
     console.log(iconNew);
     let imgDiv = document.getElementById("img"); // access the image using id
     imgDiv.src = `http://openweathermap.org/img/wn/${iconNew}@2x.png`;
+    let humidity = document.querySelector(".humidity");
+    humidity.innerHTML = response.data.main.humidity;
   }
 
   function getTemp() {
@@ -62,7 +69,25 @@ function searchCity(event) {
 function noFunc(eventr) {
   eventr.preventDefault();
 }
+function showFahren(event) {
+  event.preventDefault();
+  let tempe = document.querySelector(".number");
+  tempe.innerHTML = Math.round(celsiusDeg * 1.8 + 32);
+  document.getElementById("fahrenheit").style.color = "#272142";
+  document.getElementById("cel").style.color = "#0d6efd";
+}
+function showCel(event) {
+  event.preventDefault();
+  let tempe = document.querySelector(".number");
+  tempe.innerHTML = Math.round(celsiusDeg);
+  document.getElementById("cel").style.color = "#272142";
+  document.getElementById("fahrenheit").style.color = "#0d6efd";
+}
 let form = document.querySelector(".btn");
 form.addEventListener("click", searchCity);
-let formMain = document.querySelector(".form-inline");
+let formMain = document.querySelector(".form-control");
 formMain.addEventListener("submit", noFunc);
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", showFahren);
+let celLink = document.querySelector("#cel");
+celLink.addEventListener("click", showCel);
